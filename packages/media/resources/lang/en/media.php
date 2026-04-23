b49357c873a0db0570e9d59ef0df149081e88b53
Ben <howdu@users.noreply.github.com>
2026-04-23 18:53:11 +0100

    feat(media): move media translation file to capell-media namespace

---
+++ packages/media/resources/lang/en/media.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'replace_file' => 'Replace file',
+    'replacement_file' => 'Replacement file',
+    'replace_file_success' => 'File replaced successfully.',
+];
