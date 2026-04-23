b5204349bdc4038ab21503046dc92478ff3c82dc
Ben <howdu@users.noreply.github.com>
2026-04-23 18:53:19 +0100

    feat(media): register MediaResource via admin context provider

--- packages/media/src/Providers/AdminServiceProvider.php
+++ packages/media/src/Providers/AdminServiceProvider.php
@@ -4,13 +4,23 @@ declare(strict_types=1);
 
 namespace Capell\Media\Providers;
 
+use Capell\Admin\Facades\CapellAdmin;
+use Capell\Core\Facades\CapellCore;
+use Capell\Media\Enums\ResourceEnum;
 use Illuminate\Support\ServiceProvider;
 
 final class AdminServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        //
+        if (! CapellCore::getPackage('capell-app/media')->isInstalled()) {
+            return;
+        }
+
+        CapellAdmin::registerResource(
+            ResourceEnum::Media->name,
+            class: ResourceEnum::Media->value,
+        );
     }
 
     public function register(): void
