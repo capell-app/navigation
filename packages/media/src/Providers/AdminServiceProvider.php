36ee44c9a7cb0e3daf73568760cf64b53fd14b73
Ben <howdu@users.noreply.github.com>
2026-04-23 18:51:25 +0100

    feat(media): scaffold capell-app/media package

---
+++ packages/media/src/Providers/AdminServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Capell\Media\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+final class AdminServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        //
+    }
+
+    public function register(): void
+    {
+        //
+    }
+}
---
+++ packages/media/src/Providers/MediaServiceProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Capell\Media\Providers;
+
+use Capell\Core\Enums\PackageTypeEnum;
+use Capell\Core\Facades\CapellCore;
+use Capell\Core\Support\Packages\AbstractPackageServiceProvider;
+use Composer\InstalledVersions;
+use Spatie\LaravelPackageTools\Package;
+
+class MediaServiceProvider extends AbstractPackageServiceProvider
+{
+    public static string $name = 'capell-media';
+
+    public static string $packageName = 'capell-app/media';
+
+    public static PackageTypeEnum $type = PackageTypeEnum::Plugin;
+
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name(self::$name)
+            ->hasTranslations();
+    }
+
+    public function registeringPackage(): void
+    {
+        $this->registerPackageMetadata();
+    }
+
+    private function registerPackageMetadata(): void
+    {
+        CapellCore::registerPackage(
+            static::$packageName,
+            type: static::getType(),
+            serviceProviderClass: static::class,
+            path: realpath(__DIR__ . '/../..'),
+            version: $this->getVersion(),
+            permissions: [],
+        );
+    }
+
+    private function getVersion(): string
+    {
+        if (! class_exists(InstalledVersions::class)) {
+            return 'dev';
+        }
+
+        if (! InstalledVersions::isInstalled(static::$packageName)) {
+            return 'dev';
+        }
+
+        return InstalledVersions::getPrettyVersion(static::$packageName) ?? 'dev';
+    }
+}
