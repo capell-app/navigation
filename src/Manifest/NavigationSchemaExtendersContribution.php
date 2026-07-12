84269f8802212f4dc8503a64432d139a22437049
Ben <howdu@users.noreply.github.com>
2026-07-12 07:28:21 +0100

    fix: align package compatibility with 0.0 release line

--- src/Manifest/NavigationSchemaExtendersContribution.php
+++ src/Manifest/NavigationSchemaExtendersContribution.php
@@ -10,6 +10,6 @@ final class NavigationSchemaExtendersContribution implements ExtensionContributi
 {
     public static function compatibleCapellApiVersion(): string
     {
-        return '^4.0';
+        return '^0.0';
     }
 }
